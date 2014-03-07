fe56ddb9376439a2bb31d31ab9fc19e05d7e02aa
Marcos Santanna <marcosv.santanna@hotmail.com>
2014-03-06 22:32:52 -0500

    mobile menu

--- js/main.js
+++ js/main.js
@@ -1,1 +1,33 @@
+$(document).ready(function() {
 
+	$('#h-mobileButton').click(function(event) {
+		event.preventDefault();
+		GLOBALCONTROLS.mobileMenu();
+	});
+	
+});
+
+var GLOBALCONTROLS = {
+
+	mobileMenu : function() {
+
+		if(Modernizr.csstransitions === false) {
+			if($('#h-mainMenu').hasClass('active')) {
+				$('#h-mainMenu').removeClass('active');
+				$('#h-mainMenu').slideDown();
+			} else {
+				$('#h-mainMenu').addClass('active');
+				$('#h-mainMenu').slideUp();
+			}
+
+		} else {
+			if($('#h-mainMenu').hasClass('active')) {
+				$('#h-mainMenu').removeClass('active');
+			} else {
+				$('#h-mainMenu').addClass('active');
+			}
+		}
+
+	}
+
+};
